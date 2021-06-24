37041719891ad9bde042647d3104e5baf353b407
Jpop <jjjjllears@gmail.com>
2021-06-24 22:34:17 +0800

    加入解析

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -24,11 +24,14 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegat
     
     //MARK: 地址全部填完後定位
     @IBAction func confirmButton(_ sender: Any) {
+        
+        //沒填完顯示警告
         if roadTextField.text?.isEmpty == true || allAddressTextField.text?.isEmpty == true {
             let alert = UIAlertController(title: "資料不完整", message: "請填完整地址", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             present(alert, animated: true, completion: nil)
         } else {
+            //傳回完整地址，執行定位
             totalAdress = "\(siteTextField.text ?? "")\(roadTextField.text ?? "")\(allAddressTextField.text ?? "")"
             showmap()
         }
@@ -37,6 +40,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegat
     
     let countiesArrays = ["臺北市", "新北市", "基隆市", "桃園市", "新竹縣", "新竹市", "苗栗縣", "臺中市", "彰化縣", "南投縣", "雲林縣", "嘉義縣", "嘉義市", "臺南市", "高雄市", "屏東縣", "宜蘭縣", "花蓮縣", "臺東縣", "澎湖縣", "金門縣", "連江縣", ]
     
+    //匯入從json讀取的array
     var completeArrays = Address.data
     
     var selectedCounties: String = "金門縣"
@@ -58,6 +62,8 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegat
     var x = CLLocationDegrees()
     var y = CLLocationDegrees()
     
+    
+    //viewDidLoad放置代理delegate
     override func viewDidLoad() {
         super.viewDidLoad()
         countiesTextField.delegate = self
@@ -78,18 +84,22 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegat
         roadTextField.alpha = 0
         allAddressTextField.alpha = 0
         
+        //確保陣列分解正確，先進行排序
         completeArrays.sort { address1, address2 in
             return address1.site_id.compare(address2.site_id) == .orderedAscending
             
         }
         
-        //UserDefaults.standard.set("zh", forKey: "AppleLanguages")
     }
     
+    
+    //點擊空白處會隱藏uipickerView
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
     
+    
+    //MARK: UIPickerView的設定
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         switch pickerView.tag {
         case 1:
@@ -126,6 +136,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegat
         switch pickerView.tag {
         //MARK: -縣市選單
         case 1:
+            //為應付使用者重複選擇，每次會重置
             siteTextField.alpha = 0
             roadTextField.alpha = 0
             allAddressTextField.alpha = 0
@@ -141,6 +152,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegat
             selectedCounties = countiesArrays[row]
             let total = completeArrays.count - 1
             
+            //計算被選取的縣市在哪個區間
             for n in 0...total {
                 if completeArrays[n].city == selectedCounties {
                     trueNumber += 1
@@ -153,6 +165,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegat
             }
             rangeArrays.append(trueNumber)
             
+            //讀取計算出的區間，置入新的陣列，並去掉重複的鄉鎮名稱
             let firstN = rangeArrays[0]
             let secondN = rangeArrays[1] - 1
             var anArrays = [Address]()
@@ -162,6 +175,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegat
                 anArrays.append(completeArrays[n])
             }
             
+            //保留所有路名選項
             realSite_idArrays = anArrays
             
             let total1 = anArrays.count - 1
@@ -197,14 +211,19 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegat
             
             for m in 0...site_idCount {
                 let h = countArrays[m] - 1
-                site_idArrays.append(anArrays[h])
+                site_idArrays.append(anArrays[h]) //放入第二個UIPickerView
             }
+            
+            //顯現下一個Picker
             siteTextField.alpha = 1
             
+            //picker重載入
             pickerView.reloadAllComponents()
             
         //MARK: - 鄉鎮市區選單
         case 2:
+            
+            //與第一輪配置相同
             roadTextField.alpha = 0
             roadArrays = []
             rangeArrays = []
@@ -230,7 +249,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegat
                     falseNumber += 1
                 }
             }
-            print(trueNumber)
+            print(trueNumber) //測試數字
             print(falseNumber)
             
             rangeArrays.append(trueNumber)
@@ -240,7 +259,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegat
             
             for x in 0...secondN {
                 let y = firstN + x
-                roadArrays.append(realSite_idArrays[y].road)
+                roadArrays.append(realSite_idArrays[y].road) //路名UIPickerView資料
             }
             
             roadArrays.sort { road1, road2 in
@@ -263,7 +282,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegat
 }
 
 
-
+//ＪＳＯＮ資料讀取
 extension Address {
     static var data: [Address] {
         var arrays = [Address]()
@@ -284,6 +303,7 @@ extension Address {
 extension ViewController {
     
     
+    //定位動作
     func showmap() {
         
         /*let geoCoder = CLGeocoder()
